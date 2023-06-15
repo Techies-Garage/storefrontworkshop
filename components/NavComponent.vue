b9e3f736f2e20bc6619a78148e59cda1e66a6770
st-Michael <74749737+st-Michael@users.noreply.github.com>
2023-06-15 10:59:30 +0100

    modified folder structure

---
+++ components/NavComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <div class="navlist">
+        <li></li>
+        <li></li>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.navlist{
+    width: 100%;
+}
+</style>
